35bcfc4a3ae1ffcddeb4255412774adbf1504a31
Евгения Загородных <evgeniazagorodnykh@mail.ru>
2024-02-05 22:58:01 +0300

    Add docker

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -100,7 +100,7 @@ WSGI_APPLICATION = 'foodgram_backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': '/data/db.sqlite3',
     }
 }
 
