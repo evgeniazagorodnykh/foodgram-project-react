d4cfa9fb7d37d7c9e5c8c411e9b3f3ae71a3c54a
Евгения Загородных <evgeniazagorodnykh@mail.ru>
2024-02-08 11:27:29 +0300

    Add workflow

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -9,13 +9,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-kjv1%9q!sifzj5c2j(&40^ry56g9!u^cg-dh6te58indb)(=x^'
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = str(os.getenv('DEBUG')).lower() == 'true'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = str(os.getenv('SERVERNAMES')).split()
 
 
 # Application definition
